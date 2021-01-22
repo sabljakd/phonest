a12ce731f427583b2454ec798b372160bb135e8b
sabljakd <sabljak37@gmail.com>
2021-01-22 04:14:03 +0100

    Reconstruction

--- src/Nav.vue
+++ src/Nav.vue
@@ -1,69 +1,13 @@
 
 <template>
-
   <div id="app">
-    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
-    <div id="nav">
-      <div class="logo">
-        <img src="./assets/logo.png" width="64" height="64">
-      </div>
-      <div class="links">
-      <router-link to="/">Početna</router-link>
-      <router-link to="/phones">Uređaji</router-link>
-      <router-link to="/about">O nama</router-link>  
-      <router-link to="/help">Pomoć</router-link>
-      </div>
-    </div>
+    
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
 
-
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-
-#nav{
-  font-family: 'Quicksand', sans-serif;
-  overflow: hidden;
-  height: 8vh;
-  display:flex;
-  justify-content: space-between;
-  align-items: center;
-
-  a{
-  display: inline-block;
-  color: #131315;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  background: linear-gradient(to right, midnightblue, midnightblue 50%, royalblue 50%);
-  color: royalblue;
-  overflow: hidden;
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  background-size: 200% 100%;
-  background-position: 100%;
-  transition: background-position 275ms ease;
-  }
-  a:hover {
-  background-position: 0 100%;
-}
-}
-
-
-
-
-.logo{
-  padding-left: 15px;
-  padding-top: 5px;
-}
 
 
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,69 +1,25 @@
 <template>
 
-<div class="column">
+<div class="grid">
   <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <div class="top" style="background-color:#FFF;">
-    <div class="img">
-     <!-- <img src="../assets/phone.png" alt="Home Photo"> -->
+  <div class="content">
+    <div class="logo-grid">
+      <img src="../assets/logo.png" width="64" height="64">
     </div>
-      <div class="first">
-        <div class="one-first">
-        <a>ODLIČNA USLUGA</a><br>
-        </div>
-        <div class="two-first">
-        <h2>Ne znaš koji mobitel odabrati? Mi smo tu da ti pomognemo.</h2>
-        </div>
-        <div class="three-first">
-        <a>Jednostavno je, u par klikova odaberi željene specifikacije, klikni potvrdi i <b>Voilà</b> pronašao si željene uređaje!</a>
-        </div>
-        
+    <div class="links-grid">
+      <router-link to="/">Početna</router-link>
+      <router-link to="/phones">Uređaji</router-link>
+      <router-link to="/about">O nama</router-link>  
+      <router-link to="/help">Pomoć</router-link>
+    </div>
+    <div class="intro-grid">
+      <div class="intro-grid-left"></div>
+      <div class="intro-grid-right"></div>
+    </div>
   </div>
-  </div>
-
-  <div class="middle">
-    <div class="left-middle"><a>ZNAČAJKE</a></div>
-  </div>
-
-  <div class="bottom">
-    <div class="one-bottom">
-      <div class="left-one">
-        <h3>PRAĆENJE TRENDOVA</h3>
-        <a>Proces kupovanja može se podijeliti u više faza, 
-        od planiranja kupnje preko odabira trgovine, odabira proizvoda, plaćanja, 
-        do korištenja proizvoda i evaluacije iskustva i zadovoljstva proizvodom, 
-        a počinje puno prije odlaska u trgovinu.</a>
-
-      </div>
-      <div class="right-one">
-      </div>
-      </div>
-    <div class="two-bottom">      
-      <div class="left-two"></div>
-      <div class="right-two"><a>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      Rem placeat vitae repellendus vero dolorem, modi laborum quae explicabo 
-      architecto dicta necessitatibus in sit quos accusamus,
-       quia vel, dolor consequuntur? Velit!</a></div>
-      </div>
-    <div class="three-bottom">
-      <div class="left-three"><a>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      Rem placeat vitae repellendus vero dolorem, modi laborum quae explicabo 
-      architecto dicta necessitatibus in sit quos accusamus,
-       quia vel, dolor consequuntur? Velit!</a></div>
-      <div class="right-three"></div>
-      </div>
-    <div class="four-bottom">
-      <div class="left-four"></div>
-      <div class="right-four"><a>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      Rem placeat vitae repellendus vero dolorem, modi laborum quae explicabo 
-      architecto dicta necessitatibus in sit quos accusamus,
-       quia vel, dolor consequuntur? Velit!</a></div>
-      </div>
 </div>
-</div>
-
-
 </template>
 
 <script>
@@ -71,155 +27,93 @@
 </script>
 
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-.column{
-  
+.wrapper{
+  width: 100%;
+}
+
+.grid{
+  	background-color: #9c7597;
+		background-image: url("../assets/overlay.png"), -moz-linear-gradient(45deg, #2c3e50 15%, #3498db 85%);
+		background-image: url("../assets/overlay.png"), -webkit-linear-gradient(45deg, #2c3e50 15%, #3498db 85%);
+		background-image: url("../assets/overlay.png"), -ms-linear-gradient(45deg, #2c3e50 15%, #3498db 85%);
+		background-image: url("../assets/overlay.png"), linear-gradient(45deg, #2c3e50 15%, #3498db 85%);
+    display: grid;
+    grid-template-rows: 100vh;
+    font-family: 'Quicksand', sans-serif;
+  .content{
+    display: grid;
+    grid-template-rows: 20vh 10vh 50vh;
+    margin: 10vh 20vh 10vh 20vh;
+    .logo-grid{
+      margin: auto;
+    }
+    .links-grid{
+        border-radius: 25px;
+        background: rgba(240, 230, 140, 0.274);
+        background-repeat: repeat;
+        padding: 1px 1px 1px 1px;
+        font-family: 'Quicksand', sans-serif;
+        margin:auto;
+      a{
+        display: inline-block;
+        text-align: center;
+        text-decoration: none;
+        color: rgb(255, 255, 255);
+        padding: 20px;
+        font-size: 100%;
+      }
+    }
+    .intro-grid{
+      display: grid;
+      grid-template-columns: 50% 50%;
+      border-radius: 25px;
+      background: rgba(240, 230, 140, 0.486);
+    }
+  }
+}
+
+// Uređaji 992px ili manji //
+@media only screen and (max-width:992px){
+  .wrapper{
+    width:892px;
+  }
+  .grid{
   display: grid;
-  grid-template-rows: 72vh 5vh 170vh;
   font-family: 'Quicksand', sans-serif;
-
-.first{
-  
-  margin: 100px;
-  padding-right: 90vh;
-  a{
-    color:#232A34;
+  .content{
+    display: grid;
+    grid-template-rows: 10vh 10vh;
+    margin: 5vh 10vh 50vh 10vh;
   }
-  h2{
-    font-size: 50px;
-  }
-  
+}
 }
 
-.middle{
+// Uređaji 400px ili manji //
+@media only screen and (max-width: 600px){
+  .wrapper{
+    width:600px;
+  }
+  .grid{
   display: grid;
-  grid-template-columns: 50% 50%;
-.left-middle{
-    margin-left: 100px;
-  a{
-    color:#232A34;
+  font-family: 'Quicksand', sans-serif;
+  .content{
+    display: grid;
+    grid-template-rows: 10vh 10vh;
+    margin: 0.5vh 0.5vh 0.5vh 0.5vh;
+    .links-grid{
+      a{
+        padding:10px;
+      }
+    }
   }
 }
 }
-
-.bottom{
-      display: grid;
-      grid-template-columns: 100%;
-      grid-template-rows: 42vh 40vh 40vh 40vh;
-      background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
-  
-    .one-bottom{
-      background-color:#fbfbfbad;
-      margin: 20px 500px 20px 500px;
-      display: grid;
-      grid-template-columns: 50% 50%;
-      .right-one{
-      background-image: url("../assets/info.png");
-      background-repeat: no-repeat;
-      background-position: center;
-      background-size: cover;
-      }
-      .left-one{
-      padding: 20px;
-      text-align: center;
-      h3{
-        margin-bottom: 20px;
-        color:#232A34;
-      }
-      }
-      
-    }
-
-    .two-bottom{
-      background-color:#fbfbfbad;
-      margin: 20px 500px 20px 500px;
-      display: grid;
-      grid-template-columns: 50% 50%;
-      .left-two{
-      background-image: url("../assets/love.png");
-      background-repeat: no-repeat;
-      background-position: center;
-      background-size: cover;
-      }
-      .right-two{
-      padding: 20px;
-      text-align: center;
-      }
-    }
-
-    .three-bottom{
-      background-color:#fbfbfbad;
-      margin: 20px 500px 20px 500px;
-      display: grid;
-      grid-template-columns: 50% 50%;
-      .right-three{
-      background-image: url("../assets/help.png");
-      background-repeat: no-repeat;
-      background-position: center;
-      background-size: cover;
-      }
-      .left-three{
-      padding: 20px;
-      }
-    }
-
-    .four-bottom{
-      background-color:#fbfbfbad;
-      margin: 20px 500px 20px 500px;
-      display: grid;
-      grid-template-columns: 50% 50%;
-      .left-four{
-      background-image: url("../assets/chat.png");
-      background-repeat: no-repeat;
-      background-position: center;
-      background-size: cover;
-      }
-      .right-four{
-      padding: 20px;
-      }
-    }
-
-}
-}
-body::-webkit-scrollbar {
-  width: 0.5em;
-}
- 
-body::-webkit-scrollbar-thumb {
-  
-  background-color: #00B0FF;
-  outline: 1px solid slategrey;
-}
-
-@media screen and (max-width: 959px) {
-      
-      .column{
-        width: 100%;
-      }
-      .first{
-        width: 100%;
-      }
-      .middle{
-        width: 50%;
-      }
-      .bottom{
-        width: 100%;
-      }
-    
-      
-      
-      
-     
-     
-
-}
-
-
-
-
-
-
-  
 
 
 
